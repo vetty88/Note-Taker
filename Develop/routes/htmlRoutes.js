9cee8e0f2e2e3c4cfcfe33346b95ca7ace1287d2
Vetty <vetty88@gmail.com>
2020-07-27 18:34:40 +1000

    updated directory paths - necessary files together. Removed redundant css meta info and notes file.

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -3,20 +3,20 @@ const path = require('path');
 
 module.exports = function(app){
     app.get("/notes", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/notes.html"))
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
     app.get("/assets/js/index.js", function(req, res){
         res.sendFile(path.join(__dirname, "../public/assets/js/index.js")); 
     });
 
-    app.get("/assets/css/style.css", function(req, res){
+    app.get("/assets/css/styles.css", function(req, res){
         res.sendFile(path.join(__dirname, "../public/assets/css/styles.css")); 
     });
 
     app.get("*", function(req,res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
-    })
+    });
 
     
-}
+};
--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 //Importing get/post/delete functions
 require("./routes/apiRoutes")(app);
